da924ce19a9b821fa02956c9e757f38a0df0afb1
Jean Patimeteeporn <jpatimet@gmail.com>
2025-01-07 17:28:46 -0600

    added cursive font to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Playwrite_FR_Trad } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const cursive = Playwrite_FR_Trad({
+  variable: "--playwrite",
+  style: "normal",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${cursive.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>cousin vino</h1>
+        <h1 className="font-[family-name:var(--playwrite)]">cousin vino</h1>
         <h1>drink calculator</h1>
         <h2>
           a handy dandy calculator to help you figure out how much alcohol to
