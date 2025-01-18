afa360a7781892e69d7a922c8fe188f74528673c
Jean Patimeteeporn <jpatimet@gmail.com>
2025-01-18 01:42:06 -0600

    created utility function to calculate sparkling wines

--- src/components/utils/calculations.js
+++ src/components/utils/calculations.js
@@ -11,3 +11,17 @@ export const calculateStillWine750ml = (
   );
   return bottlesNeeded750;
 };
+
+export const calculateSparklingWine750ml = (
+  drinkerPercentage,
+  numGuests,
+  drinkAverage,
+  eventHours
+) => {
+  const glassesPer750 = 6;
+  const bottlesNeeded750 = Math.ceil(
+    ((drinkerPercentage / 100.0) * numGuests * drinkAverage * eventHours) /
+      glassesPer750
+  );
+  return bottlesNeeded750;
+};
