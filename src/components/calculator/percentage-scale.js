4fa1d19fbed59b56784f8eb0eefcae8fc456fed3
Jean Patimeteeporn <jpatimet@gmail.com>
2025-02-09 23:56:45 -0600

    removed combined view

--- src/components/calculator/percentage-scale.js
+++ src/components/calculator/percentage-scale.js
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { SliderSelection } from "./slider-selection";
-import { CombinedSlider } from "./slider-views/combined-slider";
+import { calculateStillWine750ml } from "../utils/calculations";
 
 export const PercentageScale = ({
   selectedDrinks,
@@ -16,6 +15,23 @@ export const PercentageScale = ({
     () => Object.keys(selectedDrinks).filter((drink) => selectedDrinks[drink]),
     [selectedDrinks]
   );
+
+  useEffect(() => {
+    if (selectedDrinks.redWine) {
+      const redWineNeeded = calculateStillWine750ml(
+        drinks.redWine.percentage,
+        numGuests,
+        drinks.redWine.average,
+        eventHours
+      );
+      updateDrink("redWine", "needed", redWineNeeded);
+    }
+  }, [
+    numGuests,
+    eventHours,
+    drinks.redWine.percentage,
+    drinks.redWine.average,
+  ]);
 
   // initialize percentages equally when checkboxes are clicked
   useEffect(() => {
@@ -113,80 +129,62 @@ export const PercentageScale = ({
 
   return (
     <div id="scale-container" className="flex flex-col">
-      <SliderSelection
-        handleRadioChange={(e) => setSliderChecked(e.target.value)}
-      />
-      {sliderChecked === "split" && (
-        <section
-          id="split-view"
-          className="flex flex-col justify-center text-md"
-        >
-          {selectedBeverageKeys.map((beverage, index) => (
-            <fieldset key={beverage} className="flex flex-wrap m-3">
-              <label htmlFor={beverage} className="mr-5 w-full">
-                {drinks[beverage].name || ""}
-              </label>
-              <label>
-                <input
-                  id={`${beverage}-percentage`}
-                  type="number"
-                  step="any"
-                  value={drinks[beverage].percentage || 0}
-                  onChange={(e) =>
-                    handlePercentageChange(index, Number(e.target.value))
-                  }
-                  className="ml-1 mt-1 border border-emerald-500 p-2 rounded w-[4rem]"
-                />
-                %
-              </label>
+      <section id="split-view" className="flex flex-col justify-center text-md">
+        {selectedBeverageKeys.map((beverage, index) => (
+          <fieldset key={beverage} className="flex flex-wrap m-3">
+            <label htmlFor={beverage} className="mr-5 w-full">
+              {drinks[beverage].name || ""}
+            </label>
+            <label>
               <input
-                type="range"
-                min={0}
-                max={100}
-                step={1}
+                id={`${beverage}-percentage`}
+                type="number"
+                step="any"
                 value={drinks[beverage].percentage || 0}
                 onChange={(e) =>
                   handlePercentageChange(index, Number(e.target.value))
                 }
-                disabled={drinks[beverage].locked}
-                className="w-full"
+                className="ml-1 mt-1 border border-emerald-500 p-2 rounded w-[4rem]"
               />
-              <label>
-                <input
-                  id={`${beverage}-average`}
-                  type="number"
-                  step="any"
-                  value={drinks[beverage].average || 0}
-                  onChange={(e) =>
-                    updateDrink(beverage, "average", Number(e.target.value))
-                  }
-                  className="ml-1 mt-1 border border-emerald-500 p-2 rounded w-[3rem]"
-                />
-                drinks/hour
-              </label>
-              <label className="flex items-center ml-2">
-                <input
-                  type="checkbox"
-                  checked={drinks[beverage]?.locked || false}
-                  onChange={() => toggleLock(beverage)}
-                  className=""
-                />
-                Lock
-              </label>
-            </fieldset>
-          ))}
-        </section>
-      )}
-      <CombinedSlider
-        sliderChecked={sliderChecked}
-        updateDrink={updateDrink}
-        numGuests={numGuests}
-        eventHours={eventHours}
-        selectedBeverageKeys={selectedBeverageKeys}
-        drinks={drinks}
-        setDrinks={setDrinks}
-        selectedDrinks={selectedDrinks}
-      />
+              %
+            </label>
+            <input
+              type="range"
+              min={0}
+              max={100}
+              step={1}
+              value={drinks[beverage].percentage || 0}
+              onChange={(e) =>
+                handlePercentageChange(index, Number(e.target.value))
+              }
+              disabled={drinks[beverage].locked}
+              className="w-full"
+            />
+            <label>
+              <input
+                id={`${beverage}-average`}
+                type="number"
+                step="any"
+                value={drinks[beverage].average || 0}
+                onChange={(e) =>
+                  updateDrink(beverage, "average", Number(e.target.value))
+                }
+                className="ml-1 mt-1 border border-emerald-500 p-2 rounded w-[3rem]"
+              />
+              drinks/hour
+            </label>
+            <label className="flex items-center ml-2">
+              <input
+                type="checkbox"
+                checked={drinks[beverage]?.locked || false}
+                onChange={() => toggleLock(beverage)}
+                className=""
+              />
+              Lock
+            </label>
+          </fieldset>
+        ))}
+      </section>
     </div>
   );
 };
