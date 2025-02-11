9cfbecec171799853a3f6e8159ad7d459268362b
Jean Patimeteeporn <jpatimet@gmail.com>
2025-02-11 09:36:00 -0600

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Playwrite_FR_Trad, Chakra_Petch } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const cursive = Playwrite_FR_Trad({
   variable: "--playwrite",
@@ -17,6 +19,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${cursive.variable}  ${digital.variable}  antialiased`}>
         {children}
+        <SpeedInsights />;
+        <Analytics />
       </body>
     </html>
   );
