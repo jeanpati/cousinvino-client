897f74c190e1b9708ae447b505ea9c1b9544a179
Jean Patimeteeporn <jpatimet@gmail.com>
2025-02-21 18:23:48 -0600

    updated lock position

--- src/components/calculator/percentage-scale.js
+++ src/components/calculator/percentage-scale.js
@@ -348,7 +348,7 @@ export const PercentageScale = ({
                 guests)
               </p>
             </label>
-            <div className="flex min-w-[5rem]">
+            <div className="flex min-w-[6rem] justify-center">
               <label className="flex items-center ml-1"> Lock</label>
               <input
                 type="checkbox"
