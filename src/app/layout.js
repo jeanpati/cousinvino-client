ee90ee6d7e8a3890427651ef75166e8050459303
Jean Patimeteeporn <jpatimet@gmail.com>
2025-03-14 19:11:14 -0500

    moved next Head to RootLayout and added swap property to fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,14 @@ import { Playwrite_FR_Trad, Zen_Maru_Gothic } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const cursive = Playwrite_FR_Trad({
   variable: "--playwrite",
   weight: "400",
   style: "normal",
   preload: true,
+  display: "swap",
 });
 
 const digital = Zen_Maru_Gothic({
@@ -16,11 +18,28 @@ const digital = Zen_Maru_Gothic({
   weight: "900",
   style: "normal",
   preload: true,
+  display: "swap",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title> Cousin Vino&apos;s drink calculator</title>
+        <meta property="og:title" content="Cousin Vino's Drink Calculator" />
+        <meta
+          property="og:description"
+          content="Easily calculate how many drinks to buy for your event with Cousin Vino's drink calculator. Perfect for parties, weddings, and gatherings!"
+        />
+        <meta property="og:image" content="/images/champagne-1920px.webp" />
+        <meta property="og:url" content="https://www.cousinvino.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="keywords"
+          content="drink calculator, event drink calculator, how many drinks to buy, party drinks, wedding drink calculator, drink planning, party planning"
+        />
+      </Head>
       <body
         className={`${cursive.variable}  ${digital.variable} grid place-items-center`}
       >
--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,13 @@
 import { EventDetails } from "@/components/calculator/event-details";
-import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-w-screen items-center justify-center px-4">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title> Cousin Vino&apos;s drink calculator</title>
-        <meta property="og:title" content="Cousin Vino's Drink Calculator" />
-        <meta
-          property="og:description"
-          content="Easily calculate how many drinks to buy for your event with Cousin Vino's drink calculator. Perfect for parties, weddings, and gatherings!"
-        />
-        <meta property="og:image" content="/images/champagne-1920px.webp" />
-        <meta property="og:url" content="https://www.cousinvino.com" />
-        <meta property="og:type" content="website" />
-        <meta
-          name="keywords"
-          content="drink calculator, event drink calculator, how many drinks to buy, party drinks, wedding drink calculator, drink planning, party planning"
-        />
-      </Head>
-
       <header className="flex flex-col items-center pt-4">
         <h1 className="text-center text-4xl/loose font-[family-name:var(--playwrite)]">
           Cousin Vino&apos;s drink calculator
         </h1>
-
         <Image
           src="/images/champagne-1920px.webp"
           alt="champagne bottle logo"
